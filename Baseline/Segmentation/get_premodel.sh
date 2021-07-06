a5933bf6de3489c74f5b79a64273007485d663ac
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-07 02:38:26 +0800

    Downloading models

--- Baseline/Segmentation/get_premodel.sh
+++ Baseline/Segmentation/get_premodel.sh
@@ -2,4 +2,4 @@ mkdir Models
 
 wget https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar -O ./Models/swav_800.pth
 wget https://dl.fbaipublicfiles.com/deepcluster/swav_400ep_2x224_pretrain.pth.tar -O ./Models/swav_noMC.pth
-
+wget https://cloudstor.aarnet.edu.au/plus/s/hdAg5RYm8NNM2QP/download -O ./Models/DenseCL.pth
