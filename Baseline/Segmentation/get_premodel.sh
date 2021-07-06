10bcbc3f9e827168001a600533ec3d1485d2a860
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-07 01:23:47 +0800

    Update Model Download

--- Baseline/Segmentation/get_premodel.sh
+++ Baseline/Segmentation/get_premodel.sh
@@ -1,4 +1,5 @@
-wget https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar -O ./Models/swav_800.tar
-tar xvf ./Models/swav_800.tar 
-wget https://dl.fbaipublicfiles.com/deepcluster/swav_400ep_2x224_pretrain.pth.tar -O ./Models/swav_noMC.tar
-tar xvf ./Models/swav_noMC.tar
+mkdir Models
+
+wget https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar -O ./Models/swav_800.pth
+wget https://dl.fbaipublicfiles.com/deepcluster/swav_400ep_2x224_pretrain.pth.tar -O ./Models/swav_noMC.pth
+
