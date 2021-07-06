4b7f266c8cc850b1f8ded2f62f81d2fdfc415ad5
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-07 02:36:03 +0800

    Add DenseCL

---
+++ Baseline/Segmentation/DenseCL/openselfsup/datasets/data_sources/__init__.py
@@ -0,0 +1,7 @@
+from .cifar import Cifar10, Cifar100
+from .coco import COCO
+from .image_list import ImageList
+from .imagenet import ImageNet
+from .imagenet_lmdb import ImageNetLMDB
+from .places205 import Places205
+from .coco_lmdb import CocoLMDB
