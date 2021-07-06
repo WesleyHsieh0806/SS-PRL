10fb744e6159a33777df86f431a750b65a451da6
WesleyHsieh <b06901053@ntu.edu.tw>
2021-07-07 00:41:21 +0800

    Update SwAV Code

---
+++ Baseline/Segmentation/get_premodel.sh
@@ -0,0 +1,4 @@
+wget https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar -O ./Models/swav_800.tar
+tar xvf ./Models/swav_800.tar 
+wget https://dl.fbaipublicfiles.com/deepcluster/swav_400ep_2x224_pretrain.pth.tar -O ./Models/swav_noMC.tar
+tar xvf ./Models/swav_noMC.tar
