94e9d426bdf85170bc4a601c7b91106124ee0d32
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-07 17:52:58 +0800

    GET VOC dataset

--- Baseline/Segmentation/get_VOC.sh
+++ Baseline/Segmentation/get_VOC.sh
@@ -5,7 +5,7 @@ if [ "$DATA" == "" ]; then
     exit
 fi
 # -P DIR --> save files in DIR
-#wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar -P $DATA
-#wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz -P $DATA --no-check-certificate
-#tar -xf $DATA/VOCtrainval_11-May-2012.tar -C $DATA
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar -P $DATA
+wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz -P $DATA --no-check-certificate
+tar -xf $DATA/VOCtrainval_11-May-2012.tar -C $DATA
 tar -zxvf $DATA/benchmark.tgz -C $DATA
