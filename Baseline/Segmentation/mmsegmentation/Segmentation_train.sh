2dd68f7da0feb6a05f0c71f3012dc8d0f9a6da5f
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-10 15:56:44 +0800

    Swav Segmentation

--- Baseline/Segmentation/mmsegmentation/Segmentation_train.sh
+++ Baseline/Segmentation/mmsegmentation/Segmentation_train.sh
@@ -2,9 +2,9 @@
 #PBS -l select=1:ncpus=8:ngpus=1
 #PBS -q ee
 
-#source activate b06901053
-#cd $PBS_O_WORKDIR
-#module load cuda/cuda-9.2/x86_64
+source activate b06901053
+cd $PBS_O_WORKDIR
+module load cuda/cuda-9.2/x86_64
 
 # Modify this line
 CONFIG_FILE=configs/densecl/fcn_r50-d8_512x512_20k_voc12aug.py
@@ -14,4 +14,4 @@ OUTPUT_DIR=logs/swav/swav_800
 ###
 
 
-#conda deactivate
+conda deactivate
