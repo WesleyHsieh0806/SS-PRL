252bebd294f9b6abcd462adb6a2190f753475a9b
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-11 14:55:32 +0800

    Modify configuration

--- Baseline/Segmentation/mmsegmentation/Segmentation_train.sh
+++ Baseline/Segmentation/mmsegmentation/Segmentation_train.sh
@@ -2,14 +2,15 @@
 #PBS -l select=1:ncpus=8:ngpus=1
 #PBS -q ee
 
+############### 1. modify the name of vir-env
 source activate b06901053
 cd $PBS_O_WORKDIR
 module load cuda/cuda-9.2/x86_64
 
-# Modify this line
+######################
 CONFIG_FILE=configs/densecl/fcn_r50-d8_512x512_20k_voc12aug.py
 GPUS=4
-# Change the output dir and the path of pretrained model in fcn_r50-d8.py
+# 2. Change the output dir 3. Change the path of pretrained model in fcn_r50-d8.py
 OUTPUT_DIR=logs/DenseCL
 ./tools/dist_train.sh ${CONFIG_FILE} ${GPUS} --work-dir ${OUTPUT_DIR}
 ###
