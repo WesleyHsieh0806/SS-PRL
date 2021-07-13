4763ac1dbb6d5553709b412bb58aaceb42f4f377
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-14 03:07:15 +0800

    Swav models modify

--- Baseline/Segmentation/modify_swav_models.py
+++ Baseline/Segmentation/modify_swav_models.py
@@ -6,6 +6,8 @@ parser = argparse.ArgumentParser(
     description="Modify swav pretrained files into correct format")
 parser.add_argument("--pretrained", default="", type=str,
                     help="path to pretrained weights")
+parser.add_argument("--model", default="", type=str,
+                    help="path to pretrained weights")
 args = parser.parse_args()
 
 if os.path.isfile(args.pretrained):
@@ -15,7 +17,15 @@ if os.path.isfile(args.pretrained):
 
     else:
         state_dict = model
+    # Remove module
+    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
 
-    for k, v in state_dict.items():
-        print(k)
+    densecl_model = torch.load(args.model)
+    for k, v in densecl_model.state_dict().items():
+        if k not in list(state_dict):
+            print('key "{}" could not be found in provided state dict'.format(k))
+        elif state_dict[k].shape != v.shape:
+            print(
+                'key "{}" is of different shape in model and provided state dict'.format(k))
+
     save_dict = {"state_dict", state_dict}
