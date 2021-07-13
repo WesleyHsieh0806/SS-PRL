74c5c3a8f8b7ee103f96859bf4cf0c311b39b3df
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-13 12:39:12 +0800

    Load model

--- Baseline/Segmentation/mmsegmentation/configs/densecl/fcn_r50-d8.py
+++ Baseline/Segmentation/mmsegmentation/configs/densecl/fcn_r50-d8.py
@@ -2,7 +2,7 @@
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
     type='EncoderDecoder',
-    pretrained='./pretrained/extracted_densecl_coco_800ep.pth',
+    pretrained='../Models/DenseCL.pth',
     backbone=dict(
         type='ResNetNormal',
         depth=50,
