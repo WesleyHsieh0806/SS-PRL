306099ad473f045db9697705b9fdfde60af51050
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-25 17:51:12 +0800

    Imagenet

--- get_ImageNet.sh
+++ get_ImageNet.sh
@@ -18,6 +18,8 @@ wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz -P $DAT
 
 mkdir $DATA/train
 tar -xf $DATA/ILSVRC2012_img_train.tar -C $DATA/train
+rm $DATA/ILSVRC2012_img_train.tar
 
 mkdir $DATA/val
 tar -xf $DATA/ILSVRC2012_img_val.tar -C $DATA/val
+rm $DATA/ILSVRC2012_img_val.tar
