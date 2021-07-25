a7f57e7a39f787b2e59069f726a024b8de136f58
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-07-25 23:42:10 +0800

    ImageNet Processing

--- imagenet_preprocess.sh
+++ imagenet_preprocess.sh
@@ -7,6 +7,17 @@ if [ "$DATA" == "" ]; then
     echo "Usage: bash ./get_ImageNet.sh YOUR_DATA_ROOT"
     exit
 fi
+
+# Unzip tar
+mkdir $DATA/train
+tar -xf $DATA/ILSVRC2012_img_train.tar -C $DATA/train
+rm $DATA/ILSVRC2012_img_train.tar
+
+mkdir $DATA/val
+tar -xf $DATA/ILSVRC2012_img_val.tar -C $DATA/val
+rm $DATA/ILSVRC2012_img_val.tar
+
+
 # Preprocess *.tar 
 cd $DATA/train
 find . -name "*.tar"|while read NAE; 
