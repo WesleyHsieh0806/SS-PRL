dca76d21c2306fd459ac5c7e7820e7cf1eaf4117
1999ADEK <58595755+1999ADEK@users.noreply.github.com>
2021-08-11 19:18:26 +0800

    Add script for downloading Mini-ImageNet dataset

---
+++ get_MiniImageNet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=8:ngpus=1
+#PBS -q ee
+#PBS -l walltime=24:00:00
+########## Mini-ImageNet Dataset Generation ##########
+# Please modify the environment
+source activate b06201018_swav
+cd $PBS_O_WORKDIR
+
+# Feel free to modify the data directory
+DATA="./MiniImageNet"
+mkdir $DATA
+
+# Specify the path to ILSVRC2012_img_train.tar
+TARFILE="ILSVRC2012_img_train.tar"
+
+# Clone the repository
+git clone https://github.com/yaoyao-liu/mini-imagenet-tools.git
+cd mini-imagenet-tools
+
+# Download its provided tar file if yours do not work
+#at-get a306397ccf9c2ead27155983c254227c0fd938e2
+
+# Generate Mini-ImageNet dataset
+python mini_imagenet_generator.py --tar_dir $TARFILE --image_resize 0
+
+# Move processed images to specified folder
+cd ..
+mv mini-imagenet-tools/processed_images $DATA
+rm -rf mini-imagenet-tools/
+
+conda deactivate
