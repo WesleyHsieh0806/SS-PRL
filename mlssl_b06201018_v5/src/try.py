2d6fc871b0e427635b0049d794d88c4aa1f58658
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-10-30 14:50:15 +0800

    Delete unecessary test script

--- mlssl_b06201018_v5/src/try.py
+++
@@ -1,6 +0,0 @@
-import torch
-grid_per_side = [3, 5]
-cum_nofgrid = torch.cumsum(torch.Tensor([2*(nof_grid**2)
-                                         for nof_grid in grid_per_side]), 0)
-local_offset = 8
-print([(local_offset+cum_nofgrid[i-1], local_offset+cum_nofgrid[i]) if i!=0 else (local_offset, local_offset+cum_nofgrid[i]) for i in range(len(grid_per_side))])
