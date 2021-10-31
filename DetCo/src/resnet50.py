163eecf4326dc414074eb87545274ee04bdb076f
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-10-31 21:46:58 +0800

    Debug:Infinte Loop

--- DetCo/src/resnet50.py
+++ DetCo/src/resnet50.py
@@ -154,6 +154,146 @@ class MLP(nn.Module):
         return x
 
 
+class MoResnet(nn.Module):
+
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classes: int = 128,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation=None,
+        norm_layer=None
+    ) -> None:
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+
+        self.layers = nn.ModuleList(
+            [self.layer1, self.layer2, self.layer3, self.layer4])
+
+        self.global_mlps, self.local_mlps = nn.ModuleList(), nn.ModuleList()
+        init_in_channels = 64 * block.expansion
+        for i in range(4):
+            self.global_mlps += [MLP(init_in_channels * 2 ** i, num_classes)]
+            self.local_mlps += [MLP(init_in_channels *
+                                    2 ** i, num_classes, False)]
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    # type: ignore[arg-type]
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    # type: ignore[arg-type]
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self):
+        """
+        Momentum update of the key encoder
+        """
+        for param_q, param_k in zip(self.parameters(), self.encoder_k.parameters()):
+            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys):
+        # gather keys before updating queue
+        keys = concat_all_gather(keys)
+
+        batch_size = keys.shape[0]
+
+        ptr = int(self.queue_ptr)
+        assert self.K % batch_size == 0  # for simplicity
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.queue[:, :, ptr:ptr + batch_size] = keys.permute(1, 2, 0)
+        ptr = (ptr + batch_size) % self.K  # move pointer
+
+        self.queue_ptr[0] = ptr
+
+    def _make_layer(self, block, planes: int, blocks: int,
+                    stride: int = 1, dilate: bool = False) -> nn.Sequential:
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x, is_global=True):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        mlps = self.global_mlps if is_global else self.local_mlps
+
+        reps = []
+        for layer, mlp in zip(self.layers, mlps):
+            x = layer(x)  # (Batch, c, h, w) or (9*Batch, c, h, w)
+            x1 = rearrange(x, '(n b) c h w -> b (n c) h w',
+                           n=1 if is_global else 9)
+            reps += [mlp(x1)]
+
+        return torch.stack(reps, dim=1)
+
+
 class ResNet(nn.Module):
 
     def __init__(
@@ -233,15 +373,15 @@ class ResNet(nn.Module):
         self.T = 0.07
         self.n = 8  # 4 global vectors and 4 local vectors
         # Another encoder k
-        self.encoder_k = ResNet(block,
-                                layers,
-                                num_classes,
-                                zero_init_residual,
-                                groups,
-                                width_per_group,
-                                replace_stride_with_dilation,
-                                norm_layer=None
-                                )
+        self.encoder_k = MoResnet(block,
+                                  layers,
+                                  num_classes,
+                                  zero_init_residual,
+                                  groups,
+                                  width_per_group,
+                                  replace_stride_with_dilation,
+                                  norm_layer=None
+                                  )
 
         for param_q, param_k in zip(self.parameters(), self.encoder_k.parameters()):
             param_k.data.copy_(param_q.data)  # initialize
