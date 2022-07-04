2f736838cf76acf363a33c25005ed3328ccfebdd
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2022-07-04 17:10:33 +0800

    Get pretrained models

---
+++ get_premodel.sh
@@ -0,0 +1,29 @@
+mkdir -p Models
+
+# Get SSPRL
+gdown https://drive.google.com/uc?id=1wJoi5OsVHZUYAj-7VT6hxpqAL3gKyM_Y -O ./Models/SSPRL_COCO.pth
+gdown https://drive.google.com/uc?id=1AmCCZU4Lip4J5ZqarOPyHdW2JUqMtR3O -O ./Models/SSPRL_IN.pth
+
+# Get SwAV
+gdown https://drive.google.com/uc?id=1T3nygB8LScJWQhQvYKcgDditlBnM36e_ -O ./Models/SwAV_COCO.pth
+gdown https://drive.google.com/uc?id=1wmZuaoySqPlFPguBfeKwpjzlHK4Zax2X -O ./Models/SwAV_IN.pth
+
+# Get DenseCL
+gdown https://drive.google.com/uc?id=1U4LqQfKLpVgU2rU_Ccy7kAdMSIsLuKQr -O ./Models/DenseCL_COCO.pth
+gdown https://drive.google.com/uc?id=1Xcr87gLs7ObmnJtggDTf1iIhpjAi061O -O ./Models/DenseCL_IN.pth
+
+# Get BYOL
+gdown https://drive.google.com/uc?id=1rm9SvAWRwxORLmMPM4cM2wV0W4-uZizi -O ./Models/BYOL_COCO.pth
+gdown https://drive.google.com/uc?id=1i1Ruaa7ieIGrs5i4l7rVtTFHXS9coqU5 -O ./Models/BYOL_IN.pth
+
+# Get InsLoc
+gdown https://drive.google.com/uc?id=1SLfY7VpMHC0ppYuc57wrpV-LAWaZUBtT -O ./Models/InsLoc_COCO.pth
+gdown https://drive.google.com/uc?id=1xMrXSY3i0mPDo1O6gWTXp8YPliWkTcYs -O ./Models/InsLoc_IN.pth
+
+# Get MaskCo
+gdown https://drive.google.com/uc?id=1Zv_YiLmoYzViuxNVw9cYp-tyRGNSCj5q -O ./Models/MaskCo_COCO.pth
+gdown https://drive.google.com/uc?id=1PewwYdcAfamuZ41mwYeSSoqD6rIZhOBS -O ./Models/MaskCO_IN.pth
+
+# Get MoCo
+gdown https://drive.google.com/uc?id=1Bz7qDeC6mDSWck3MZ9w0ZQO_tneG-ZFo -O ./Models/MoCo_COCO.pth
+gdown https://drive.google.com/uc?id=1PDetV1ZtnmaRuEHB7fVQdVwLERrc0WC3 -O ./Models/MoCo_IN.pth
