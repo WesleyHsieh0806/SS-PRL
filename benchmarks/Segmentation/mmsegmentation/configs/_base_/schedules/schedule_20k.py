1c3367131e9b8403ef3574a09080a374451af0a0
Cheng-Yen Hsieh <55971907+WesleyHsieh0806@users.noreply.github.com>
2022-08-30 03:02:58 -0400

    Update schedule_20k.py

--- benchmarks/Segmentation/mmsegmentation/configs/_base_/schedules/schedule_20k.py
+++ benchmarks/Segmentation/mmsegmentation/configs/_base_/schedules/schedule_20k.py
@@ -1,5 +1,5 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.05, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
